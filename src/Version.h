674b88214dbfd1b0160510d38f80d663b40d38a5
jrap1245 <jc.rapin@gmail.com>
2026-03-06 18:02:48 +0100

    Ton message

--- src/Version.h
+++ src/Version.h
@@ -7,5 +7,6 @@
  *  - New LogLevel OLED
  *  - getLastLines() return only OLED and greater LogLevel's
  */
-/* Version 1.1.1 record only displayLogLevel and + in circuilar buffer*/
-#define HSTLOG_VERSION "1.1.1"
+/* Version 1.1.1 record only displayLogLevel and + in circular buffer*/
+/* Version 1.2.0 added pagination to log requests*/
+#define HSTLOG_VERSION "1.2.0"
